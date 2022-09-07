0aa2e30f7e3b805ccf35fe484b9bfa10dcc0f308
min <minyore@naver.com>
2022-09-07 11:03:04 +0900

    네비게이션 메뉴

--- proto/module/mod_navi.js
+++ proto/module/mod_navi.js
@@ -1,25 +1,16 @@
-var navOffset = $(".com_navi").offset();
-$(window).scroll(function () {
-    if ($(document).scrollTop() > 30) {
-    $(".com_navi, .com_navi_header").addClass("fixed");
-    } else {
-    $(".com_navi, .com_navi_header").removeClass("fixed");
-    }
+$(".com_navi").simpleMenu({
+    stickyMenu: true,
+    slidingLine: true,
+    slidingLineColor: 'transparent',
+    pageNavigationSpeedAnimate: 500,
+    winMobWidth: 0,
 });
 
-$("ul.navi > li").click(function () {
-    var items = $(this).attr("data-tab");
-    $("ul.navi > li").removeClass("active");
-    $(this).addClass("active");
-    if ($(".com_navi, .com_navi_header").hasClass("fixed")) {
-    $("html, body").animate(
-        { scrollTop: $("#" + items + "").offset().top - 171 },
-        "500"
-    );
-    } else {
-    $("html, body").animate(
-        { scrollTop: $("#" + items + "").offset().top - 422 },
-        "500"
-    );
-    }
+var navOffset = $(".com_navi_header").offset().top;
+$(window).scroll(function () {
+  if ($(document).scrollTop() > navOffset) {
+    $(".com_navi_header").addClass("fixed");
+  } else {
+    $(".com_navi_header").removeClass("fixed");
+  }
 });
