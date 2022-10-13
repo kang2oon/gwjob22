a67cc9ee603091f66cfdf329ce9c87cfb3fbc617
aldo814 <66293497+aldo814@users.noreply.github.com>
2022-10-13 12:59:01 +0900

    직업선호도진단

--- jnjob/html/js/diag.js
+++ jnjob/html/js/diag.js
@@ -1,15 +1,4 @@
-function openModal(modalname) {
-	document.get
-	$("." + modalname).show();
-	$('body').addClass('not_scroll');
-	$('.shadow').show();
-}
 
-function close_pop(flag) {
-	$('.diag_pop').hide();
-	$('body').removeClass('not_scroll');
-	$('.shadow').hide();
-};
 $(document).ready(function () {
 	// 직업선호도 진단
 	$('.job_selct a').click(function () {
@@ -25,3 +14,16 @@ $(document).ready(function () {
 	});
 
 });
+
+function openModal(modalname) {
+	document.get
+	$("." + modalname).show();
+	$('body').addClass('not_scroll');
+	$('.shadow').show();
+}
+
+function close_pop(flag) {
+	$('.diag_pop').hide();
+	$('body').removeClass('not_scroll');
+	$('.shadow').hide();
+};
